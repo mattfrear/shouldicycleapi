6389b6789a781b857b0461f067feae9d8acf634d
Matt Frear <matt.frear@gmail.com>
2014-09-16 16:34:14 +0100

    Replaced tabs with spaces

--- server.js
+++ server.js
@@ -17,50 +17,50 @@ var router = express.Router(); 				// get an instance of the express Router
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next(); // make sure we go to the next routes and don't stop here
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our cycle api!' });	
+    res.json({ message: 'hooray! welcome to our cycle api!' });	
 });
 
 router.get('/v1/cycle', function(req, res, next) {
-	var location = req.query.location;
-	var airQualities = req.query.airquality;
-	var shouldicycle = {};
-	shouldicycle.pollution = [];
+    var location = req.query.location;
+    var airQualities = req.query.airquality;
+    var shouldicycle = {};
+    shouldicycle.pollution = [];
 
-	async.parallel([
-		// Get weather
-		function(callback) {
-			if (location) {
-				weather.lookupCurrentWeather(location, shouldicycle, callback);
-			}
-			else {
-				callback();
-			}
-		},
-		// Get airquality
-		function(callback) {
-			if (airQualities) {
-				var airQualityArray = airQualities.split(',');
-			    async.forEach(airQualityArray, function(airQuality, airQualityCallback) {
-			        weather.lookupAirQuality(airQuality, shouldicycle, airQualityCallback);
-			    }, callback);
-			}
-			else {
-				callback();
-			}
-		}
-	], 
-	// This is called when weather and airquality are done
-	function(err) {
-		if (err) return next(err);
-		res.json(shouldicycle);	
-	});
+    async.parallel([
+        // Get weather
+        function(callback) {
+            if (location) {
+                weather.lookupCurrentWeather(location, shouldicycle, callback);
+            }
+            else {
+                callback();
+            }
+        },
+        // Get airquality
+        function(callback) {
+            if (airQualities) {
+                var airQualityArray = airQualities.split(',');
+                async.forEach(airQualityArray, function(airQuality, airQualityCallback) {
+                    weather.lookupAirQuality(airQuality, shouldicycle, airQualityCallback);
+                }, callback);
+            }
+            else {
+                callback();
+            }
+        }
+    ], 
+    // This is called when weather and airquality are done
+    function(err) {
+        if (err) return next(err);
+        res.json(shouldicycle);	
+    });
 });
 
 // REGISTER OUR ROUTES -------------------------------
